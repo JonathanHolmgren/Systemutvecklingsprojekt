e30e0a07511dff55eb6ba9661953af68161f6593
huggsterbigboss <hugocarlsen@icloud.com>
2024-10-03 15:17:56 +0200

    Created a UoW Class. Co-authored-by: LinusOlsson <s2205286@student.hb.se> Co-authored-by: Yodel99 <joelahlstrom@outlook.com>

---
+++ DataLayer/UnitOfWork.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataLayer
+{
+    public class UnitOfWork
+    {
+        private bool isDisposed = false;
+        private readonly bool disposeContext = false;
+        Context Context {  get; }
+
+        public CommisionRateRepository CommisionRateRepository { get; private set; }
+        public CompanyCustomerRepository CompanyCustomerRepository { get; private set;}
+        public CustomerRepository CustomerRepository { get; private set; }
+        public EmployeeRepository EmployeeRepository { get; private set; }
+        public InsuranceRepository InsuranceRepository { get; private set; }
+        public InsuranceSpecRepository InsuranceSpecRepository { get; private set; }
+        public InsuranceTypeAttributeRepository InsuranceTypeAttributeRepository { get; private set; }
+        public InsuranceTypeRepository InsuranceTypeRepository { get; private set; }
+        public InsuredPersonRepository InsuredPersonRepository { get; private set; }
+        public PostalCodeCityRepository PostalCodeCityRepository { get; private set;}
+        public PrivateCustomerRepository PrivateCustomerRepository { get; private set; }
+        public UserRepository UserRepository { get; private set; }
+
+
+        public UnitOfWork(Context context)
+        {
+            Context = context;
+            CommisionRateRepository = new CommisionRateRepository(context);
+            CompanyCustomerRepository = new CompanyCustomerRepository(context);
+            CustomerRepository = new CustomerRepository(context);
+            EmployeeRepository = new EmployeeRepository(context);
+            InsuranceRepository = new InsuranceRepository(context);
+            InsuranceSpecRepository = new InsuranceSpecRepository(context);
+            InsuranceTypeAttributeRepository = new InsuranceTypeAttributeRepository(context);
+            InsuranceTypeRepository = new InsuranceTypeRepository(context);
+            InsuredPersonRepository = new InsuredPersonRepository(context);
+            PostalCodeCityRepository = new PostalCodeCityRepository(context);
+            PrivateCustomerRepository = new PrivateCustomerRepository(context);
+            UserRepository = new UserRepository(context);
+        }
+        public UnitOfWork() : this(new Context())
+        {
+            disposeContext = true;
+        }
+        public int SaveChanges()
+        {
+            try
+            {
+                return Context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            catch (RetryLimitExceededException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+    
+}

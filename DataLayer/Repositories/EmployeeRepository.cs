667f23614fd7513322f03d145733556c93136dda
LinusOlsson <s2205286@student.hb.se>
2024-10-03 14:51:08 +0200

    Co-authored-by: hugocarlsen01 <s2206999@student.hb.se> Co-authored-by: Yodel99 <joelahlstrom@outlook.com>

---
+++ DataLayer/Repositories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>
+    {
+        public EmployeeRepository(Context context) : base(context) { }
+    }
+}

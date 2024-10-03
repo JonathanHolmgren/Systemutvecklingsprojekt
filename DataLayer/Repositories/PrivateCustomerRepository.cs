5f19cc91bdc3a2b9204fcf8581c414e32b810034
LinusOlsson <s2205286@student.hb.se>
2024-10-03 14:51:08 +0200

    Co-authored-by: hugocarlsen01 <s2206999@student.hb.se> Co-authored-by: Yodel99 <joelahlstrom@outlook.com>

---
+++ DataLayer/Repositories/PrivateCustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataLayer.Repositories
+{
+    public class PrivateCustomerRepository : Repository<PrivateCustomer>
+    {
+        public PrivateCustomerRepository(Context context) : base(context) { }
+    }
+}

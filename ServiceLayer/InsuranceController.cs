8bdee447374c3ab71bccffc1913253561baa81de
Jakob Vallared <Jakobw03@hotmail.se>
2024-10-14 13:16:11 +0200

    Added AddInsurance method and identified missing Update() in UnitOfWork.

--- ServiceLayer/InsuranceController.cs
+++ ServiceLayer/InsuranceController.cs
@@ -16,14 +16,33 @@ namespace ServiceLayer
             DateTime expiryDate,
             BillingInterval billingInterval,
             User user,
-            InsuranceStatus insuranceStatus,
             InsuranceType insuranceType,
             string notes,
             Customer customer,
-            InsuredPerson insuredPerson
+            InsuredPerson insuredPerson //Måste skapas en metod för att skapa detta objektet. Finns inte med i backlogen just nu. 
         )
         {
-            return new Insurance(expiryDate, billingInterval, user, insuranceStatus, insuranceType, notes, customer, insuredPerson);
+            return new Insurance(
+                expiryDate,
+                billingInterval,
+                user,
+                insuranceType,
+                notes,
+                customer,
+                insuredPerson
+            );
+        }
+
+        public void AddInsurance(Insurance insurance)
+        {
+            unitOfWork.InsuranceRepository.Add(insurance);
+            //Vi har inte metoden Update i unitofwork
+            //t.ex;
+            //unitOfWork.Update(insurance.User)
+            //unitOfWork.Update(insurance.Customer)
+            //unitOfWork.Update(insurance.InsuredPerson)
+            //unitOfWork.Update(insurance.InsuredType)
+            unitOfWork.SaveChanges();
         }
     }
 }

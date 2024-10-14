54b14506217311af37c8a2eb9785821e22e322bd
Jakob Vallared <Jakobw03@hotmail.se>
2024-10-14 12:24:34 +0200

    Have made a controller for Insurance and made a method to register a preliminary insurance in the controller.

---
+++ ServiceLayer/InsuranceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using Models;
+
+namespace ServiceLayer
+{
+    public class InsuranceController
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        public Insurance RegisterPreliminaryInsurance(
+            DateTime expiryDate,
+            BillingInterval billingInterval,
+            User user,
+            InsuranceStatus insuranceStatus,
+            InsuranceType insuranceType,
+            string notes,
+            Customer customer,
+            InsuredPerson insuredPerson
+        )
+        {
+            return new Insurance(expiryDate, billingInterval, user, insuranceStatus, insuranceType, notes, customer, insuredPerson);
+        }
+    }
+}

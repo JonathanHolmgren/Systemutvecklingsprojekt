0c7e9fe64a854f112e7c3502903bb47ef638ef0d
Noahryhd1 <s2205911@student.hb.se>
2024-10-14 12:59:11 +0200

    Create a view for calculation of comission and viewmodels

---
+++ PresentationLayer/ViewModels/CalculateComissionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using PresentationLayer.Command;
+
+namespace PresentationLayer.ViewModels
+{
+    public class CalculateComissionViewModel : ObservableObject
+    {
+        public CalculateComissionViewModel() { }
+    }
+}

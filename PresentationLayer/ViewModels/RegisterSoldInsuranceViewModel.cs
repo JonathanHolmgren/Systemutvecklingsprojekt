be4c12ad05097ba207c32b4409b324a1da4c268f
Joel Ahlström <joelahlstrom@outlook.com>
2024-10-14 14:57:40 +0200

    Did a binding from datagrid in view ti viewmodel where i have a temporary hardcoded list

--- PresentationLayer/ViewModels/RegisterSoldInsuranceViewModel.cs
+++ PresentationLayer/ViewModels/RegisterSoldInsuranceViewModel.cs
@@ -4,11 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
+using System.ComponentModel;
 
 namespace PresentationLayer.ViewModels
 {
     public class RegisterSoldInsuranceViewModel: ObservableObject
     {
+        Insurance testInsurance1 = new Insurance(InsuranceStatus.Active);
+        Insurance testInsurance2 = new Insurance(InsuranceStatus.Preliminary);
+
+        private IList<Insurance> _testInsurances;
+        public IList<Insurance> TestInsurance
+        {
+            get => _testInsurances;
+            set
+            {
+                _testInsurances = value;
+                OnPropertyChanged(nameof(TestInsurance));
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public RegisterSoldInsuranceViewModel()
+        {
+            
+           
+            TestInsurance = new List<Insurance> { testInsurance1, testInsurance2 };
+        }
 
     }
 }

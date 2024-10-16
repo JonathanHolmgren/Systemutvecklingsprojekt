298419ba14cf763f850ed28c8b071d9090c55e06
Jakob Vallared <Jakobw03@hotmail.se>
2024-10-16 12:17:34 +0200

    Made a ViewModel for RegisterPreliminaryInsurance

---
+++ PresentationLayer/ViewModels/RegisterPreliminaryInsuranceViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using PresentationLayer.Command;
+using PresentationLayer.Models;
+using ServiceLayer;
+
+namespace PresentationLayer.ViewModels
+{
+    public class RegisterPreliminaryInsuranceViewModel:ObservableObject
+    {
+        InsuranceController insuranceController;
+
+        public RegisterPreliminaryInsuranceViewModel()
+        {
+            insuranceController = new InsuranceController();
+            LoadCustomerData();
+        }
+
+        private PrivateCustomer privateCustomer;
+        public PrivateCustomer PrivateCustomer
+        {
+            get => PrivateCustomer;
+            set 
+            {
+                privateCustomer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        PostalCodeCity postalCodeCity = new PostalCodeCity
+        {
+            City = "Borås",
+            PostalCode = "50630"
+        };
+
+        private void LoadCustomerData()
+        {
+            PrivateCustomer privateCustomer1 = new PrivateCustomer(
+            "0706689932",
+            "ingalillblommor52@emial.com",
+            "Gatuvägen 21",
+            postalCodeCity,
+            "19521019-1234",
+            "Inga-Lill",
+            "Bengtsson"
+        );
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}

50f39e8b4886f089caee56af96037c1b6696f6de
huggsterbigboss <hugocarlsen@icloud.com>
2024-10-17 11:25:31 +0200

    Fixed so that i can show private or company customer

--- PresentationLayer/ViewModels/ShowProspectsViewModel.cs
+++ PresentationLayer/ViewModels/ShowProspectsViewModel.cs
@@ -17,6 +17,7 @@ namespace PresentationLayer.ViewModels
         private CustomerController customerController = new CustomerController();
 
         private ObservableCollection<Customer> customers = null;
+        
         public ObservableCollection<Customer> Customers
         {
             get { return customers; }
@@ -37,6 +38,68 @@ namespace PresentationLayer.ViewModels
                 OnPropertyChanged(nameof(Prospects)); 
             }
         }
+        private ObservableCollection<Customer> privateProspects = null;
+        public ObservableCollection<Customer> PrivateProspects
+        {
+            get { return privateProspects; }
+            set
+            {
+                privateProspects = value;
+                OnPropertyChanged(nameof(PrivateProspects));
+            }
+        }
+        private ObservableCollection<Customer> companyProspects = null;
+        public ObservableCollection<Customer> CompanyProspects
+        {
+            get { return companyProspects; }
+            set
+            {
+                companyProspects = value;
+                OnPropertyChanged(nameof(CompanyProspects));
+            }
+        }
+
+        private bool isAllSelected;
+        public bool IsAllSelected
+        {
+            get { return isAllSelected; }
+            set
+            {
+                isAllSelected = value;
+                OnPropertyChanged(nameof(IsAllSelected));
+                UpdateProspectsList();
+            }
+        }
+
+        private bool isCompanySelected;
+        public bool IsCompanySelected
+        {
+            get { return isCompanySelected; }
+            set
+            {
+                isCompanySelected = value;
+                OnPropertyChanged(nameof(IsCompanySelected));
+                OnPropertyChanged(nameof(IsCompanyColumnVisible));
+                OnPropertyChanged(nameof(IsPrivateColumnVisible));
+                UpdateProspectsList();
+            }
+        }
+
+        private bool isPrivateSelected;
+        public bool IsPrivateSelected
+        {
+            get { return isPrivateSelected; }
+            set
+            {
+                isPrivateSelected = value;
+                OnPropertyChanged(nameof(IsPrivateSelected));
+                OnPropertyChanged(nameof(IsCompanyColumnVisible));
+                OnPropertyChanged(nameof(IsPrivateColumnVisible));
+                UpdateProspectsList();
+            }
+        }
+        public bool IsPrivateColumnVisible => IsPrivateSelected;
+        public bool IsCompanyColumnVisible => IsCompanySelected;
 
         private Customer prospectSelectedItem;
         public Customer ProspectSelectedItem 
@@ -48,20 +111,28 @@ namespace PresentationLayer.ViewModels
                 OnPropertyChanged(nameof(ProspectSelectedItem));
             }
         }
+
         public ICommand ShowProspectCommand { get; private set; }
         public ICommand ReturnCommand { get; private set; }
         public ShowProspectsViewModel()
         {
             Customers = new ObservableCollection<Customer>(customerController.GetAllCustomers());
+            
             Prospects = new ObservableCollection<Customer>();
             Prospects = FilterProspects();
+            PrivateProspects = new ObservableCollection<Customer>();
+            PrivateProspects = FilterPrivateProspects();
+            CompanyProspects = new ObservableCollection<Customer>();
+            CompanyProspects = FilterCompanyProspects();
+
             ShowProspectCommand = new RelayCommand(ShowProspect);
             ReturnCommand = new RelayCommand(Return);
-
         }
 
+        #region Methods
         private ObservableCollection<Customer> FilterProspects()
         {
+            Prospects.Clear();
             foreach (var customer in Customers)
             {
                 if (customer.Insurances != null && customer.Insurances.Count == 1)
@@ -72,13 +143,64 @@ namespace PresentationLayer.ViewModels
             return Prospects;
         }
 
+        private ObservableCollection<Customer> FilterPrivateProspects()
+        {
+            PrivateProspects.Clear();
+            foreach (var customer in Customers)
+            {
+                if (customer is PrivateCustomer privateCustomer)
+                {
+                    if (privateCustomer.Insurances != null && privateCustomer.Insurances.Count == 1)
+                    {
+                        PrivateProspects.Add(privateCustomer);
+                    }
+                }
+            }
+            return PrivateProspects;
+        }
+
+        private ObservableCollection<Customer> FilterCompanyProspects()
+        {
+            CompanyProspects.Clear();
+            foreach (var customer in Customers)
+            {
+                if (customer is CompanyCustomer companyCustomer)
+                {
+                    if (customer.Insurances != null && customer.Insurances.Count == 1)
+                    {
+                        CompanyProspects.Add(companyCustomer);
+                    }
+                }
+            }
+            return CompanyProspects;
+        }
+        private void UpdateProspectsList()
+        {
+            if (IsCompanySelected)
+            {                
+                Prospects = FilterCompanyProspects();
+            }
+            else if (IsPrivateSelected)
+            {                
+                Prospects = FilterPrivateProspects();
+            }
+            else if (IsAllSelected)
+            {                
+                Prospects = FilterProspects();
+            }
+        }
+
+        //Show a more detailed view of the prospect
         private void ShowProspect()
         {
 
         }
-        private void Return()
-        {
 
+        //Return to main menu
+        private void Return() 
+        {
+            
         }
+        #endregion
     }
 }

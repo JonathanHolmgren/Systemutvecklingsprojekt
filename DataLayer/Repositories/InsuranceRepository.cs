24e774bdfd53d990be88624d437bb9e620bfb46b
Noahryhd1 <s2205911@student.hb.se>
2024-10-18 13:33:51 +0200

    Update insuranceRepository

--- DataLayer/Repositories/InsuranceRepository.cs
+++ DataLayer/Repositories/InsuranceRepository.cs
@@ -12,47 +12,6 @@ namespace DataLayer.Repositories
     {
         public InsuranceRepository(Context context) : base(context) { }
 
-        public double GetTotalPremiumForLastMonth(Employee employee)
-        {
-            using (var context = new Context()) 
-            {
-                var lastMonthStart = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
-                var lastMonthEnd = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddDays(-1);
-
-                var insurancesSoldLastMonth = context.Insurances 
-                    .Include(i => i.User) 
-                    .Where(i => i.User.Employee.AgentNumber == employee.AgentNumber &&
-                                i.ExpiryDate >= lastMonthStart &&
-                                i.ExpiryDate <= lastMonthEnd)
-                    .ToList();
-
-                if (!insurancesSoldLastMonth.Any())
-                {
-                    return 0;
-                }
-
-                double totalPremium = 0;
-
-                foreach (var insurance in insurancesSoldLastMonth)
-                {
-                    var insuranceSpecs = context.InsuranceSpecs 
-                        .Where(spec => spec.Insurance.InsuranceId == insurance.InsuranceId &&
-                                       spec.InsuranceTypeAttribute != null &&
-                                       spec.InsuranceTypeAttribute.InsuranceAttribute == "Månadspremie")
-                        .ToList();
-
-                    foreach (var spec in insuranceSpecs)
-                    {
-                        if (double.TryParse(spec.Value, out double premium))
-                        {
-                            totalPremium += premium; 
-                        }
-                    }
-                }
-
-                return totalPremium;
-            }
-        }
 
         public double GetTotalPremiumForPeriod(Employee employee, DateTime startDate, DateTime endDate)
         {

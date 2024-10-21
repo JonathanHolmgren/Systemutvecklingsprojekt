f0a47ae150bcbeabdb8c6fef6d80db1fdadfcbbc
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-10-21 14:23:21 +0200

    change the datatype on UserName to string and not String.

--- Models/User.cs
+++ Models/User.cs
@@ -10,7 +10,7 @@ namespace Models
     public class User
     {
         public int UserID { get; set; }
-        public String UserName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public AuthorizationLevel AuthorizationLevel { get; set; }
 

dcdb8f6611806befd9587ef30422b2a5d92dcaae
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-10-21 12:31:08 +0200

    Add validation for login.

---
+++ ServiceLayer/LoginUser.cs
@@ -0,0 +1,29 @@
+﻿using DataLayer;
+using Models;
+
+namespace ServiceLayer;
+
+public class LoginUser
+{
+    UnitOfWork unitOfWork = new UnitOfWork();
+    PasswordHasher passwordHasher = new PasswordHasher();
+
+    public User ValidateUser(string username, string password)
+    {
+        User? user = unitOfWork.UserRepository.FirstOrDefault(u => u.UserName == username);
+
+        if (user is null)
+        {
+            throw new Exception("User was not found");
+        }
+
+        bool verified = passwordHasher.Verify(password, user.Password);
+
+        if (!verified)
+        {
+            throw new Exception("Invalid password");
+        }
+
+        return user;
+    }
+}

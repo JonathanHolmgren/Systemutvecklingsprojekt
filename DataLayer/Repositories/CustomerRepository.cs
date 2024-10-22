bc7735f827dbf5f156cf076d96fbc5d10d44498c
Noahryhd1 <s2205911@student.hb.se>
2024-10-22 13:48:46 +0200

    Update

--- DataLayer/Repositories/CustomerRepository.cs
+++ DataLayer/Repositories/CustomerRepository.cs
@@ -11,16 +11,6 @@ namespace DataLayer.Repositories
     public class CustomerRepository : Repository<Customer>
     {
         public CustomerRepository(Context context) : base(context) { }
-
-        public List<Customer> GetInActiveCustomers()
-        {
-            DateTime oneYearAgo = DateTime.Now.AddYears(-1);
-
-            return Context.Set<Customer>()
-                .Where(c => !c.Insurances.Any(i => i.ExpiryDate > oneYearAgo && i.InsuranceStatus != InsuranceStatus.Active))
-                .ToList();
-        }
-
        
         public List<Customer> GetInActiveCustomersWithInsurances()
         {

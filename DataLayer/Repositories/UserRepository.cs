4bf54edbd6f47e96e76e2e31ee2d8512ed30e694
Noahryhd1 <s2205911@student.hb.se>
2024-10-22 12:25:38 +0200

    Create logic for controlling authorization

--- DataLayer/Repositories/UserRepository.cs
+++ DataLayer/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,11 @@ namespace DataLayer.Repositories
     public class UserRepository : Repository<User>
     {
         public UserRepository(Context context) : base(context) { }
+
+        public User GetUserByCredentials(string username, string password)
+        {
+            return Context.Set<User>().FirstOrDefault(u => u.Employee.AgentNumber == username && u.Password == password);
+        }
+
     }
 }
---
+++ ServiceLayer/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DataLayer.Repositories;
+
+namespace ServiceLayer
+{
+    public class UserController
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        public AuthorizationLevel GetAuthorizationLevelForUser(string username, string password)
+        {
+            var user = unitOfWork.UserRepository.GetUserByCredentials(username, password);
+            if (user != null)
+            {
+                return user.AuthorizationLevel;
+            }
+
+            throw new Exception("Invalid login credentials.");
+        }
+
+    }
+}

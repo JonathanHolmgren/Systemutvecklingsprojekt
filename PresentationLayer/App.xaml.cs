208a05bb3e1c98296395e41ac7ecd8d711b9f91d
LinusOlsson <s2205286@student.hb.se>
2024-10-23 15:13:08 +0200

    MainWindow View

--- PresentationLayer/App.xaml.cs
+++ PresentationLayer/App.xaml.cs
@@ -19,39 +19,40 @@ namespace PresentationLayer
 
         protected override void OnStartup(StartupEventArgs e)
         {
+
             var services = new ServiceCollection();
             ConfigureServices(services);
             ServiceProvider = services.BuildServiceProvider();
 
             // Skapa MainWindow från DI-container och visa det
             var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
-            mainWindow.Show();
+            
         }
 
-        //private void ConfigureServices(ServiceCollection services)
-        //{
-        //    // Ladda konfiguration från appsettings.json
-        //    var configuration = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        //        .Build();
+        private void ConfigureServices(ServiceCollection services)
+        {
+            //// Ladda konfiguration från appsettings.json
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //    .Build();
 
-        //    // Registrera IConfiguration för DI
-        //    services.AddSingleton<IConfiguration>(configuration);
+            //// Registrera IConfiguration för DI
+            //services.AddSingleton<IConfiguration>(configuration);
 
-        //    // Registrera DbContext
-        //    services.AddDbContext<Context>(options =>
-        //    {
-        //        var connectionString = configuration.GetConnectionString("toppforsakringar");
-        //        options.UseSqlServer(connectionString);
-        //    });
+            //// Registrera DbContext
+            //services.AddDbContext<Context>(options =>
+            //{
+            //    var connectionString = configuration.GetConnectionString("toppforsakringar");
+            //    options.UseSqlServer(connectionString);
+            //});
 
-        //    // Registrera MyService så den kan få Context via DI
-        //    services.AddTransient<MyService>();
+            //// Registrera MyService så den kan få Context via DI
+            //services.AddTransient<MyService>();
 
-        //    // Registrera MainWindow
-        //    services.AddTransient<MainWindow>();
-        //}
+            // Registrera MainWindow
+            services.AddTransient<MainWindow>();
+        }
     }
 
 }
---
+++ PresentationLayer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PresentationLayer.Command;
+using PresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using PresentationLayer.Views;
+using System.Diagnostics;
+
+namespace PresentationLayer.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+
+        // Close App
+
+        private UserControl _currentView;
+
+        public UserControl CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+
+        // Close App Command
+        private ICommand _closeCommand;
+
+        public ICommand CloseAppCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                {
+                    _closeCommand = new RelayCommand<object>(execute => CloseApp(execute));
+                }
+                return _closeCommand;
+            }
+        }
+        public ICommand ChangeViewCommand { get; }
+        public MainWindowViewModel()
+        {
+            
+            CurrentView = new ExportBillingInformationView(); // Startvy 
+            ChangeViewCommand = new RelayCommand<Type>(ChangeViewByType);
+
+        }
+        // Maximize App
+        public void MaxApp(object obj)
+        {
+            MainWindow win = obj as MainWindow;
+
+            if (win.WindowState == WindowState.Normal)
+            {
+                win.WindowState = WindowState.Maximized;
+            }
+            else if (win.WindowState == WindowState.Maximized)
+            {
+                win.WindowState = WindowState.Normal;
+            }
+        }
+        private void ChangeViewByType(Type viewType)
+        {
+            if (viewType != null)
+            {
+                CurrentView = (UserControl)Activator.CreateInstance(viewType);
+                Debug.WriteLine($"New view loaded: {viewType.Name}");
+            }
+        }
+        public void ChangeView(UserControl newView)
+        {
+            CurrentView = newView;
+        }
+
+
+        // Maximize App Command
+        private ICommand _maxCommand;
+        public ICommand MaxAppCommand
+        {
+            get
+            {
+                if (_maxCommand == null)
+                {
+                    _maxCommand = new RelayCommand<object>(execute => MaxApp(execute));
+                }
+                return _maxCommand;
+            }
+        }
+
+
+        public void CloseApp(object obj)
+        {
+            MainWindow win = obj as MainWindow;
+            win.Close();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+
+       
+
+       
+}

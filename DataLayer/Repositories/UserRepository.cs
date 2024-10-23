6fbdcbee3731ab04db2084221a6809f85d31ffd3
huggsterbigboss <hugocarlsen@icloud.com>
2024-10-23 10:39:02 +0200

    Fixed ctor in InsuredPerson, added app.xaml.cs and User repository after accidentaly removed them while merging

---
+++ DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DataLayer.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(Context context) : base(context) { }
+
+        public User GetUserByCredentials(string username, string password)
+        {
+            return Context.Set<User>().FirstOrDefault(u => u.Employee.AgentNumber == username && u.Password == password);
+        }
+    }
+}

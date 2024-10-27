f30cc2db9b31a3dea62a2659e8824da6c1364bc9
linus <linus@DESKTOP-HK3SBHD>
2024-10-27 19:49:44 +0100

    Design

--- PresentationLayer/ViewModels/MainWindowViewModel.cs
+++ PresentationLayer/ViewModels/MainWindowViewModel.cs
@@ -12,13 +12,56 @@ using System.Windows;
 using System.Windows.Controls;
 using PresentationLayer.Views;
 using System.Diagnostics;
+using PresentationLayer.Services;
+using Models;
+using ServiceLayer;
 
 namespace PresentationLayer.ViewModels
 {
-    public class MainWindowViewModel : ObservableObject
+    public class MainWindowViewModel : ObservableObject, ICloseWindows
     {
+        private User user = null;
+        public User User
+        {
+            get { return user; }
+            set
+            {
+                if (user != value)
+                {
+                    user = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private Employee employee = null;
+        public Employee Employee
+        {
+            get { return employee; }
+            set
+            {
+                if (employee != value)
+                {
+                    employee = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private bool _isSubmenuOpen;
 
-        // Close App
+        public bool IsSubmenuOpen
+        {
+            get => _isSubmenuOpen;
+            set
+            {
+                _isSubmenuOpen = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowSubMenuCommand => new RelayCommand(() => IsSubmenuOpen = !IsSubmenuOpen);
+        public Action Close { get; set; }
+        private ICommand exitCommand = null!;
+        public ICommand ExitCommand => exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
 
         private object _currentView;
 
@@ -29,57 +72,62 @@ namespace PresentationLayer.ViewModels
             {
                 if (_currentView != value)
                 {
+                    IsSubmenuOpen = false;
                     _currentView = value;
-                    OnPropertyChanged(nameof(CurrentView)); // Viktigt för att uppdatera UI
+                    OnPropertyChanged(); 
                 }
             }
         }
 
-
-        // Close App Command
-        private ICommand _closeCommand;
-
-        public ICommand CloseAppCommand
+        private ICommand _maxCommand;
+        public ICommand MaxAppCommand
         {
             get
             {
-                if (_closeCommand == null)
+                if (_maxCommand == null)
                 {
-                    _closeCommand = new RelayCommand<object>(execute => CloseApp(execute));
+                    _maxCommand = new RelayCommand<object>(execute => MaxApp(execute));
                 }
-                return _closeCommand;
+                return _maxCommand;
             }
         }
+
+        // Close App Command
+
 
         public ICommand ChangeViewCommand { get; }
         public MainWindowViewModel()
         {
-            
+            TempController tempController= new TempController();
             CurrentView = new ExportBillingInformationView(); // Startvy 
             ChangeViewCommand = new RelayCommand<Type>(ChangeViewByType);
-
+            User = tempController.GetUserTemp(1);
+            Employee = user.Employee;
         }
-        // Maximize App
-        //public void MaxApp(object obj)
-        //{
-        //    MainWindow win = obj as MainWindow;
+        private void MaxApp(object parameter)
+        {
+            if (parameter is Window window)
+            {
+                // Kontrollera om fönstret redan är maximerat
+                if (window.WindowState == WindowState.Maximized)
+                {
+                    // Om ja, återställ till normalt läge
+                    window.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    // Annars maximera fönstret
+                    window.WindowState = WindowState.Maximized;
+                }
+            }
+        }
 
-        //    if (win.WindowState == WindowState.Normal)
-        //    {
-        //        win.WindowState = WindowState.Maximized;
-        //    }
-        //    else if (win.WindowState == WindowState.Maximized)
-        //    {
-        //        win.WindowState = WindowState.Normal;
-        //    }
-        //}
         private void ChangeViewByType(Type viewType)
         {
 
             if (viewType != null)
             {
                 CurrentView = (UserControl)Activator.CreateInstance(viewType);
-                Debug.WriteLine($"Nu kör vi: {viewType.Name}");
             }
         }
         public void ChangeView(UserControl newView)
@@ -88,26 +136,9 @@ namespace PresentationLayer.ViewModels
         }
 
 
-        // Maximize App Command
-        //private ICommand _maxCommand;
-        //public ICommand MaxAppCommand
-        //{
-        //    get
-        //    {
-        //        if (_maxCommand == null)
-        //        {
-        //            _maxCommand = new RelayCommand<object>(execute => MaxApp(execute));
-        //        }
-        //        return _maxCommand;
-        //    }
-        //}
-
-
-        public void CloseApp(object obj)
-        {
-            MainWindow win = obj as MainWindow;
-            win.Close();
-        }
+        
+       
+        
 
         public event PropertyChangedEventHandler PropertyChanged;
 

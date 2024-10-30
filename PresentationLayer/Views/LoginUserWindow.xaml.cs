fe26adc7196f95fcb2f376efd77aa5315dfaadcc
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-10-30 11:09:48 +0100

    Remove some files
    Co-authored-by: LinusOlsson <s2205286@student.hb.se>
    Co-authored-by: Yodel99 <joelahlstrom@outlook.com>

---
+++ PresentationLayer/Views/LoginUserWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace PresentationLayer.Views;
+
+public partial class LoginUserWindow : Window
+{
+    public LoginUserWindow()
+    {
+        InitializeComponent();
+    }
+}

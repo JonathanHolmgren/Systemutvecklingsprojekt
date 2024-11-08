4c648967719f5c73c7bce88cfec4616b9e9336ad
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-11-08 21:42:19 +0100

    Comment out the program SeedData

--- SeedData/Program.cs
+++ SeedData/Program.cs
@@ -2,20 +2,20 @@
 
 namespace SeedData // Note: actual namespace depends on the project name.
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Context context = new Context();
-
-            Console.WriteLine("Would you like to reset the Database? Y/N");
-
-            if (Console.ReadKey().Key == ConsoleKey.Y)
-            {
-                // context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-                Seed.Populate(context);
-            }
-        }
-    }
+    // internal class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Context context = new Context();
+    //
+    //         Console.WriteLine("Would you like to reset the Database? Y/N");
+    //
+    //         if (Console.ReadKey().Key == ConsoleKey.Y)
+    //         {
+    //             // context.Database.EnsureDeleted();
+    //             context.Database.EnsureCreated();
+    //             Seed.Populate(context);
+    //         }
+    //     }
+    // }
 }

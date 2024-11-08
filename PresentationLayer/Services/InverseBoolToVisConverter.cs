6893d43e571556a7e62971698640992f98f72ebb
jonathanholmgren <jonathan.holmgren@hotmail.com>
2024-11-08 19:44:53 +0100

    Fix: Add function so that Admin only can Remove customer in customer profile.

---
+++ PresentationLayer/Services/InverseBoolToVisConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PresentationLayer.Services;
+
+public class InverseBoolToVisConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool boolValue = (bool)value;
+        return boolValue ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
